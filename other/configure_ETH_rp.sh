526c548204315f58aff0ce2fd406d93579edcfca
Mikołaj Rogacki <mikolajrogacki@gmail.com>
2021-05-25 15:37:09 +0200

    Update configure_ETH_rp.sh

--- other/configure_ETH_rp.sh
+++ other/configure_ETH_rp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo touch /media/mikolaj/boot/ssh
-echo 'denyinterfaces wlan0' | sudo tee --append /media/mikolaj/rootfs/etc/dhcpcd.conf
+sudo touch /media/<user>/boot/ssh
+echo 'denyinterfaces wlan0' | sudo tee --append /media/<user>/rootfs/etc/dhcpcd.conf
