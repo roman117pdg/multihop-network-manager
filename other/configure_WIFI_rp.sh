7ba3099fecc1a360dba229dba591c5174a72bfc0
Mikołaj Rogacki <mikolajrogacki@gmail.com>
2021-05-25 15:36:47 +0200

    Update configure_WIFI_rp.sh

--- other/configure_WIFI_rp.sh
+++ other/configure_WIFI_rp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo touch /media/mikolaj/boot/ssh
-sudo touch /media/mikolaj/boot/wpa_supplicant.conf
+sudo touch /media/<user>/boot/ssh
+sudo touch /media/<user>/boot/wpa_supplicant.conf
 echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 country=PL
@@ -11,11 +11,5 @@ network={
 	psk=\"PASSWORD\"
 	key_mgmt=WPA-PSK
 }" > /media/mikolaj/boot/wpa_supplicant.conf
-echo 'denyinterfaces wlan0' | sudo tee --append /media/mikolaj/rootfs/etc/dhcpcd.conf
-echo "raspberrypi_ANT" > /media/mikolaj/rootfs/etc/hostname
-echo "127.0.0.1	localhost
-::1		localhost ip6-localhost ip6-loopback
-ff02::1		ip6-allnodes
-ff02::2		ip6-allrouters
+echo 'denyinterfaces wlan0' | sudo tee --append /media/<user>/rootfs/etc/dhcpcd.conf
 
-127.0.1.1		raspberrypi_ANT" > /media/mikolaj/rootfs/etc/hosts 
