b7dad240cd77ec490ceb5a2ff00566968e56df3b
Mikołaj Rogacki <mikolajrogacki@gmail.com>
2021-05-27 16:20:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,6 +19,19 @@ def get_interface_list():
         interfaces_list.append(interface[1])
     return interfaces_list
 
+def is_ipv4(ipv4):
+    if len(ipv4) == 0:
+        return False
+    ipv4_array = ipv4.split(".")
+    if len(ipv4_array) != 4:
+        return False
+    for number in ipv4_array:
+        if int(number) > 255 or int(number) < 0:
+            return False
+    return True
+
+
+
 
 def main():
     """Main function for starting program. Responsible for accepting flags which describe node mode and verbosity level.
@@ -36,7 +49,8 @@ def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--verbose')
     argparser.add_argument('--webapi')
-    argparser.add_argument('--interface')
+    argparser.add_argument('--iface_babel')
+    argparser.add_argument('--iface_gateway')
     argparser.add_argument('--gateway')
     argparser.add_argument('--essid')
     argparser.add_argument('--wep_key')
@@ -52,17 +66,21 @@ def main():
         main_logger.warning("wrong input arguments: "+str(args))
         main_logger.warning("--webapi 0/1")
         sys.exit(0)
-    if args.interface!=None and args.interface not in interfaces_list:
+    if args.iface_babel!=None and args.iface_babel not in interfaces_list:
         main_logger.warning("wrong input arguments: "+str(args))
-        main_logger.warning("--interface <string name of interface>")
-        main_logger.warning("available interfaces:")
+        main_logger.warning("--iface_babel <string name of interface>")
+        main_logger.warning("available iface_babel:")
         main_logger.warning(interfaces_list)
         sys.exit(0)
-    if args.gateway!=None and args.gateway not in interfaces_list:
+    if args.iface_gateway!=None and args.iface_gateway not in interfaces_list:
         main_logger.warning("wrong input arguments: "+str(args))
-        main_logger.warning("--gateway <string name of interface>")
-        main_logger.warning("available interfaces:")
+        main_logger.warning("--iface_gateway <string name of interface>")
+        main_logger.warning("available iface_gateway:")
         main_logger.warning(interfaces_list)
+        sys.exit(0)
+    if args.gateway!=None and not is_ipv4(args.gateway):
+        main_logger.warning("wrong input arguments: "+str(args))
+        main_logger.warning("--gateway <string value of ipv4>")
         sys.exit(0)
     if args.wep_key!=None and len(args.wep_key)!=26:
         main_logger.warning("wrong input arguments: "+str(args))
@@ -79,9 +97,12 @@ def main():
     webapi = args.webapi 
     if args.webapi == None:
         webapi = '0'
-    interface = args.interface 
-    if args.interface == None:
-        interface = 'wlan0'
+    iface_babel = args.iface_babel 
+    if args.iface_babel == None:
+        iface_babel = 'wlan0'
+    iface_gateway = args.iface_gateway 
+    if args.iface_gateway == None:
+        iface_gateway = 'None'
     gateway = args.gateway 
     if args.gateway == None:
         gateway = 'None'
@@ -98,7 +119,7 @@ def main():
     if args.channel == None:
         channel = '1'
  
-    main_logger.info("starting program with agruments (verbose: "+str(verbose)+", webapi: "+str(webapi)+", interface: "+str(interface)+", gateway: "+str(gateway)+
+    main_logger.info("starting program with agruments (verbose: "+str(verbose)+", webapi: "+str(webapi)+", iface_babel: "+str(iface_babel)+", iface_gateway: "+str(iface_gateway)+", gateway: "+str(gateway)+
     "essid: "+str(essid)+", wep_key: "+str(wep_key)+", cell_id: "+str(cell_id)+", channel: "+str(channel)+")")
     
     # set verbosity level of logger
@@ -123,8 +144,8 @@ def main():
         main_logger.setLevel(logging.INFO) 
         main_logger = logger.enable_print_file(main_logger)
         main_logger = logger.enable_print_terminal(main_logger)
-
-    ah_i = adhoc_init.AdhocInit(main_logger, interface, gateway, essid, wep_key, cell_id, channel)
+        
+    ah_i = adhoc_init.AdhocInit(main_logger=main_logger, iface_babel=iface_babel, iface_gateway=iface_gateway, gateway=gateway, essid=essid, wep_key=wep_key, cell_id=cell_id, channel=channel)
     ah_i.run()
     ipv6 = ah_i.get_ipv6()
     ipv4 = ah_i.get_ipv4()
@@ -135,7 +156,7 @@ def main():
 
     
     main_logger.info("creating BabelManager")
-    bm = babel_manager.BabelManager(mac, ipv6, ipv4, interface, interface_index, sn, ah_i, main_logger)
+    bm = babel_manager.BabelManager(mac=mac, ip_v6=ipv6, ip_v4=ipv4, interface=iface_babel, iface_idx=interface_index, sn=sn, ah_i=ah_i, main_logger=main_logger)
     
     if webapi == '1':
         flask_thread = Thread(target = flask_app.run, name="flask", args =(main_logger, bm, mac, ipv6, ipv4, sn, model,  ), daemon=True)
